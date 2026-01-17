10feed6eac31545d19fd6a5737e569329642648a
Leonardo Troche <leo.m.troche@gmail.com>
2026-01-17 15:59:19 -0300

    refactor(tmdb-client): reorganize axios client configuration and improve code structure

--- apps/backend/src/shared/tmdb/tmdb-client.ts
+++ apps/backend/src/shared/tmdb/tmdb-client.ts
@@ -1,12 +1,31 @@
 import { env } from '@/config/env'
 import axios from 'axios'
+import type { TmdbMovieList } from './tmdb-types'
 
-export const tmdbClient = axios.create({
+// ------------------------------------------------------------
+// Axios Client Configuration
+// ------------------------------------------------------------
+
+export const client = axios.create({
   baseURL: env.TMDB_BASE_URL,
   params: {
     api_key: env.TMDB_API_KEY,
-    page: 1,
-    language: 'en-US',
   },
   timeout: 5000,
 })
+
+// ------------------------------------------------------------
+// TMDB Client
+// ------------------------------------------------------------
+
+export const tmdbClient = {
+  movies: {
+    getNowPlaying: (params: any) => client.get<TmdbMovieList>('/movie/now_playing', { params }),
+
+    getPopular: (params: any) => client.get<TmdbMovieList>('/movie/popular', { params }),
+
+    getTopRated: (params: any) => client.get<TmdbMovieList>('/movie/top_rated', { params }),
+
+    getUpcoming: (params: any) => client.get<TmdbMovieList>('/movie/upcoming', { params }),
+  },
+}
